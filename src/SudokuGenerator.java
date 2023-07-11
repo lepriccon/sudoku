3d80d0fdda48126adf19c30e7b82e30178484ad2
Alexey <BezAlex95@gmail.com>
2023-07-11 22:05:13 +0300

    !!

--- src/SudokuGenerator.java
+++ src/SudokuGenerator.java
@@ -2,26 +2,25 @@ import java.util.*;
 
 public class SudokuGenerator {
 
-    public static void fillArea(SudokuBoard sudokuBoard) {
-        sudokuCreator(sudokuBoard);
-    }
-
-    public static void sudokuCreator(SudokuBoard sudokuBoard) {
+    public static void generate(SudokuBoard sudokuBoard) {
         fillBoard(
                 GenerateSequence.generateSudoku(sudokuBoard.getSize(),
                         sudokuBoard.getSquareSize().size),
                 sudokuBoard);
+        sudokuBoard.createStartingCondition();
     }
 
     public static void fillBoard(int[][] body, SudokuBoard board) {
         List<Integer> squareBody = new ArrayList<>();
         int counter = 0;
-        for (int squareVert = 0; squareVert < board.getSquareSize().size; squareVert ++) {
-            for (int squareHor = 0; squareHor < board.getSquareSize().size; squareHor ++) {
+        int rowMy;
+        int collMy;
+        for (int squareVert = 0; squareVert < board.getSquareSize().size; squareVert++) {
+            for (int squareHor = 0; squareHor < board.getSquareSize().size; squareHor++) {
                 for (int row = 0; row < board.getSquareSize().size; row++) {
                     for (int col = 0; col < board.getSquareSize().size; col++) {
-                        int rowMy = row + squareVert*board.getSquareSize().size;
-                        int collMy = col + squareHor*board.getSquareSize().size;
+                        rowMy = row + squareVert * board.getSquareSize().size;
+                        collMy = col + squareHor * board.getSquareSize().size;
                         squareBody.add(body[rowMy][collMy]);
                     }
                 }
@@ -31,5 +30,24 @@ public class SudokuGenerator {
             }
         }
     }
+
+//    public static void fillBoard(int[][] body, SudokuBoard board) {
+//        List<Integer> squareBody = new ArrayList<>();
+//        int counter = 0;
+//        for (int squareVert = 0; squareVert < board.getSquareSize().size; squareVert++) {
+//            for (int squareHor = 0; squareHor < board.getSquareSize().size; squareHor++) {
+//                for (int row = 0; row < board.getSquareSize().size; row++) {
+//                    for (int col = 0; col < board.getSquareSize().size; col++) {
+//                        int rowMy = row + squareVert * board.getSquareSize().size;
+//                        int collMy = col + squareHor * board.getSquareSize().size;
+//                        squareBody.add(body[rowMy][collMy]);
+//                    }
+//                }
+//                board.getSudokuBody().get(counter).fillSquare(squareBody);
+//                counter++;
+//                squareBody.clear();
+//            }
+//        }
+//    }
 
 }
--- src/SudokuSquare.java
+++ src/SudokuSquare.java
@@ -1,41 +1,81 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class SudokuSquare {
     private final int size;
-    private boolean isFull;
-    //private final int[][] squareBody;
-    private List<Integer> body;
+    private Integer[] body;
 
-    SudokuSquare(SudokuSizes parameter) {
-        this.isFull = false;
-        this.size = parameter.size;
+    SudokuSquare(SudokuSizes sudokuSize) {
+        this.size = sudokuSize.size;
+        this.body = new Integer[size * size];
         //this.squareBody = new int[size][size];
-        this.body = Arrays.asList(new Integer[size * size]);
+        //this.body = Arrays.asList(new Integer[size * size]);
+    }
+
+    private SudokuSquare(int size) {
+        this.size = size;
+        this.body = new Integer[size * size];
+        //this.squareBody = new int[size][size];
+        //this.body = Arrays.asList(new Integer[size * size]);
     }
 
     public Integer getOne(int row, int column) {
-        return body.get((row * size) + column);
+        return body[(row * size) + column];
     }
 
     public List<Integer> getRow(int rowNumber) {
         int pointer = rowNumber * size;
-        List<Integer> row = body.subList(pointer, pointer + size);
-        return row;
+        Integer[] row = Arrays.copyOfRange(body, pointer, pointer + size);
+        return Arrays.asList(row);
     }
+
+//    public List<Integer> getRow(int row) {
+//        return Arrays.asList(body);
+//    }
 
     public List<Integer> getColumn(int columnNumber) {
         List<Integer> column = new ArrayList<>();
         for (int i = 0; i < size; i++) {
-            column.add(body.get(i * size + columnNumber));
+            column.add(body[i * size + columnNumber]);
         }
         return column;
     }
 
-    public void fillSquare(List<Integer> numbers) {
-        this.body = new ArrayList<>(numbers);
-        int i = 0;
+//    public List<Integer> getColumn(int column) {
+//        List<Integer> result = new ArrayList<>();
+//        for (int i = 0; i < size; i++) {
+//            result.add(body[i][column]);
+//        }
+//        return result;
+//    }
+
+    public SudokuSquare hideSomeNumbers() {
+        Random rand = new Random();
+        SudokuSquare result = new SudokuSquare(this.size);
+        int numbersToHide = rand.nextInt(size, size * 2);
+        Set<Integer> randomIndexes = new HashSet<>();
+        getRandomIndexes(randomIndexes, numbersToHide, rand);
+        for (Integer index : randomIndexes){
+            body[index] = 0;
+        }
+        return result;
     }
 
+    private void getRandomIndexes(Set<Integer> indexes, int setSize, Random random){
+        while (indexes.size() < setSize) {
+            if (!indexes.add(random.nextInt(size*2))){
+                getRandomIndexes(indexes, setSize, random);
+            }
+        }
+    }
+
+//    public void fillSquare(List<Integer> numbers) {
+//        this.body = new ArrayList<>(numbers);
+//        int i = 0;
+//    }
+
+    public void fillSquare(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            body[i] = numbers.get(i);
+        }
+    }
 }
